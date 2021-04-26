0f316f77a538b98df122d323e2bb078529c277e2
pennynd1me <4prilkim@gmail.com>
2021-04-26 21:50:08 +0900

    관람객을 표현하는 클래스 작성

---
+++ src/main/java/com/pennynd1me/study/_01_ticket/Audience.java
@@ -0,0 +1,10 @@
+package com.pennynd1me.study._01_ticket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/** 관람객 */
+@AllArgsConstructor
+public class Audience {
+    @Getter private Bag bag;
+}
