c70731a29de5cfc226fb0a8c0dbffa4fff7d8c08
pennynd1me <4prilkim@gmail.com>
2021-04-28 12:32:16 +0900

    리팩토링: 판매원을 자율적인 존재로 리팩토링

--- src/main/java/com/pennynd1me/study/_01_ticket/Theater.java
+++ src/main/java/com/pennynd1me/study/_01_ticket/Theater.java
@@ -10,23 +10,14 @@ public class Theater {
     /**
      * 관람객을 맞이 하는 메소드
      * @param audience  관람객
-     * if: 관람객의 가방에 초대장이 있다면
-     * 티켓 교환원이 매표소에서 티켓을 get 해서
-     * 관람객의 가방에 set 해준다.
-     * else: 관람객의 가방에 초대장이 없다면
-     * 관람객의 가방에서 티켓 가격을 차감하고
-     * 티켓 교환원의 매표소에 판매 금액을 더한 뒤
-     * 관람객의 가방에 티켓을 set 해준다.
+     * 판매원이 관람객을 맞이한다.
      */
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
+    }
+
+    // 소극장의 판매원을 테스트하기 위한 getter
+    public TicketSeller getTicketSeller() {
+        return ticketSeller;
     }
 }
